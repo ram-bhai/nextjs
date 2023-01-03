e682fc03f5517b406c06e2045dd3e4acc52d499e
Shriram Mishra <ram.techcare27@gmail.com>
2023-01-03 18:59:27 +0530

    Added table component

--- pages/maintable.js
+++ pages/maintable.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 import PropTypes from 'prop-types';
 import { alpha } from '@mui/material/styles';
 import Box from '@mui/material/Box';
+import Grid from '@mui/material/Unstable_Grid2';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -14,9 +15,12 @@ import Typography from '@mui/material/Typography';
 import Paper from '@mui/material/Paper';
 import IconButton from '@mui/material/IconButton';
 import Tooltip from '@mui/material/Tooltip';
-import DeleteIcon from '@mui/icons-material/Delete';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 import MoreHorizRoundedIcon from '@mui/icons-material/MoreHorizRounded';
 import { visuallyHidden } from '@mui/utils';
+import Checkedtable from '../components/Checkedtable';
+import Columntable from '../components/Columntable';
+import Complextable from '../components/Complextable';
 
 function createData(name, calories, fat, carbs, protein) {
   return {
@@ -29,11 +33,11 @@ function createData(name, calories, fat, carbs, protein) {
 }
 
 const rows = [
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Donut', 452, 25.0, 51, 4.9),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
+  createData('Marketplace', 305, 3.7, 67, 4.3),
+  createData('Venus DB PRO', 452, 25.0, 51, 4.9),
+  createData('Venus DS', 262, 16.0, 24, 6.0),
+  createData('Venus 3D Asset', 159, 6.0, 24, 4.0),
+  createData('Uranus', 356, 16.0, 49, 3.9),
   createData('Honeycomb', 408, 3.2, 87, 6.5),
   createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
   createData('Jelly Bean', 375, 0.0, 94, 0.0),
@@ -74,34 +78,24 @@ function stableSort(array, comparator) {
 const headCells = [
   {
     id: 'name',
-    numeric: false,
-    disablePadding: true,
-    label: 'Dessert (100g serving)',
+    
+    label: 'Title',
   },
   {
     id: 'calories',
     numeric: true,
-    disablePadding: false,
-    label: 'Calories',
+    label: 'Title',
   },
   {
     id: 'fat',
     numeric: true,
-    disablePadding: false,
-    label: 'Fat (g)',
+    label: 'Title',
   },
   {
     id: 'carbs',
     numeric: true,
-    disablePadding: false,
-    label: 'Carbs (g)',
-  },
-  {
-    id: 'protein',
-    numeric: true,
-    disablePadding: false,
-    label: 'Protein (g)',
-  },
+    label: 'Title',
+  }
 ];
 
 function EnhancedTableHead(props) {
@@ -127,6 +121,7 @@ function EnhancedTableHead(props) {
               active={orderBy === headCell.id}
               direction={orderBy === headCell.id ? order : 'asc'}
               onClick={createSortHandler(headCell.id)}
+              IconComponent={KeyboardArrowUpIcon}
             >
               {headCell.label}
               {orderBy === headCell.id ? (
@@ -143,68 +138,45 @@ function EnhancedTableHead(props) {
 }
 
 EnhancedTableHead.propTypes = {
-  numSelected: PropTypes.number.isRequired,
   onRequestSort: PropTypes.func.isRequired,
-  onSelectAllClick: PropTypes.func.isRequired,
   order: PropTypes.oneOf(['asc', 'desc']).isRequired,
   orderBy: PropTypes.string.isRequired,
   rowCount: PropTypes.number.isRequired,
 };
 
 function EnhancedTableToolbar(props) {
-  const { numSelected } = props;
-
   return (
     <Toolbar
       sx={{
         pl: { sm: 2 },
         pr: { xs: 1, sm: 1 },
-        ...(numSelected > 0 && {
-          bgcolor: (theme) =>
-            alpha(theme.palette.primary.main, theme.palette.action.activatedOpacity),
-        }),
       }}
     >
-      {numSelected > 0 ? (
-        <Typography
-          sx={{ flex: '1 1 100%' }}
-          color="inherit"
-          variant="subtitle1"
-          component="div"
-        >
-          {numSelected} selected
-        </Typography>
-      ) : (
+      {
         <Typography
           sx={{ flex: '1 1 100%' }}
           variant="h6"
           id="tableTitle"
           component="div"
-          color = "#2B3674"
+          color="#2B3674"
+          fontWeight="Bold"
         >
           Development Table
         </Typography>
-      )}
+      }
 
-      {numSelected > 0 ? (
-        <Tooltip title="Delete">
+      {<Tooltip title="More">
+        <Box sx={{
+          background: '#eaeef7',
+          borderRadius: '15px',
+          justifyContent: 'center'
+        }}>
           <IconButton>
-            <DeleteIcon />
+            <MoreHorizRoundedIcon style={{ fill: '#2B3674' }} />
           </IconButton>
-        </Tooltip>
-      ) : (
-        <Tooltip title="More">
-          <Box sx={{
-            background: '#eaeef7',
-            borderRadius: '15px',
-            justifyContent: 'center'
-          }}>
-          <IconButton>
-            <MoreHorizRoundedIcon style={{fill:'#2B3674'}}/>
-          </IconButton>
-          </Box>
-        </Tooltip>
-      )}
+        </Box>
+      </Tooltip>
+      }
     </Toolbar>
   );
 }
@@ -212,9 +184,9 @@ function EnhancedTableToolbar(props) {
 export default function EnhancedTable() {
   const [order, setOrder] = React.useState('asc');
   const [orderBy, setOrderBy] = React.useState('calories');
-  const [selected, setSelected] = React.useState([]);
+ 
   const [page] = React.useState(0);
-  const [rowsPerPage] = React.useState(5);
+  const [rowsPerPage] = React.useState(10);
 
   const handleRequestSort = (event, property) => {
     const isAsc = orderBy === property && order === 'asc';
@@ -222,66 +194,74 @@ export default function EnhancedTable() {
     setOrderBy(property);
   };
 
-  const handleSelectAllClick = (event) => {
-    if (event.target.checked) {
-      const newSelected = rows.map((n) => n.name);
-      setSelected(newSelected);
-      return;
-    }
-    setSelected([]);
-  };
+ 
 
   return (
-    <Box sx={{ width: '100%' }}>
-      <Paper sx={{ width: '100%', mb: 2 }}>
-        <EnhancedTableToolbar numSelected={selected.length} />
-        <TableContainer>
-          <Table
-            sx={{ minWidth: 750 }}
-            aria-labelledby="tableTitle"
-          >
-            <EnhancedTableHead
-              numSelected={selected.length}
-              order={order}
-              orderBy={orderBy}
-              onSelectAllClick={handleSelectAllClick}
-              onRequestSort={handleRequestSort}
-              rowCount={rows.length}
-            />
-            <TableBody>
-              {stableSort(rows, getComparator(order, orderBy))
-                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                .map((row, index) => {
-                  const labelId = `enhanced-table-checkbox-${index}`;
+    <Box sx={{padding: (theme) => theme.spacing(1)}}>
+      <Grid container rowSpacing={0} columnSpacing={{ xs: 1, sm: 2, md: 2 }}>
+        <Grid xs={6}>
+          <Paper sx={{ width: '100%', mb: 2 }}>
+            <EnhancedTableToolbar />
+            <TableContainer>
+              <Table
+                sx={{ minWidth: 550 }}
+                aria-labelledby="tableTitle"
+              >
+                <EnhancedTableHead
+                  order={order}
+                  orderBy={orderBy}
+                  onRequestSort={handleRequestSort}
+                  rowCount={rows.length}
+                />
+                <TableBody>
+                  {stableSort(rows, getComparator(order, orderBy))
+                    .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                    .map((row, index) => {
+                      const labelId = `enhanced-table-checkbox-${index}`;
 
-                  return (
-                    <TableRow
-                      hover
-                      role="checkbox"
-                      tabIndex={-1}
-                      key={row.name}
-                    >
-                      <TableCell padding="checkbox">
-                      </TableCell>
-                      <TableCell
-                        component="th"
-                        id={labelId}
-                        scope="row"
-                        padding="none"
-                      >
-                        {row.name}
-                      </TableCell>
-                      <TableCell align="right">{row.calories}</TableCell>
-                      <TableCell align="right">{row.fat}</TableCell>
-                      <TableCell align="right">{row.carbs}</TableCell>
-                      <TableCell align="right">{row.protein}</TableCell>
-                    </TableRow>
-                  );
-                })}
-            </TableBody>
-          </Table>
-        </TableContainer> 
-      </Paper>
+                      return (
+                        <TableRow
+                          hover
+                          role="checkbox"
+                          tabIndex={-1}
+                          key={row.name} 
+                        >
+                          <TableCell padding="checkbox">
+                          </TableCell>
+                          <TableCell
+                            component="th"
+                            id={labelId}
+                            scope="row"
+                            padding="none"
+                            style ={{color: "#2B3674", fontWeight:"Bold"}} 
+                          >
+                            {row.name}
+                          </TableCell>
+                          <TableCell align="right" style = {{color: "#E0E5F2", }}>{row.calories}</TableCell>
+                          <TableCell align="right" style ={{color: "#2B3674", fontWeight:"Bold"}}>{row.fat}</TableCell>
+                          <TableCell align="right" style ={{color: "#2B3674", fontWeight:"Bold"}}>{row.carbs}</TableCell>
+                          {/* <TableCell align="right" style ={{color: "#2B3674", fontWeight:"Bold"}}>{row.protein}</TableCell> */}
+                        </TableRow>
+                      );
+                    })}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </Paper>
+        </Grid>
+
+        <Grid xs={6}>
+         <Checkedtable/>
+        </Grid> 
+
+        <Grid xs={6}>
+          <Columntable/>
+        </Grid>
+
+        <Grid xs={6}>
+        <Complextable/>
+        </Grid>
+      </Grid>
     </Box>
   );
 }
