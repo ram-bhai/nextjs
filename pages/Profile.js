ae8c90cec2c0f7915a7c093177e11fcd3e0edc4b
Shriram Mishra <ram.techcare27@gmail.com>
2023-01-20 21:55:38 +0530

    color for switch

--- pages/Profile.js
+++ pages/Profile.js
@@ -37,6 +37,57 @@ const ExpandMore = styled((props) => {
   marginLeft: 'auto',
 }));
 
+const IOSSwitch = styled((props) => (
+  <Switch focusVisibleClassName=".Mui-focusVisible" disableRipple {...props} />
+))(({ theme }) => ({
+  width: 38,
+  height: 20,
+  padding: 1,
+  '& .MuiSwitch-switchBase': {
+    padding: 0,
+    margin: 2,
+    transitionDuration: '300ms',
+    '&.Mui-checked': {
+      transform: 'translateX(16px)',
+      color: '#fff',
+      '& + .MuiSwitch-track': {
+        backgroundColor: theme.palette.mode === 'dark' ? '#2ECA45' : '#4318FF',
+        opacity: 1,
+        border: 0,
+      },
+      '&.Mui-disabled + .MuiSwitch-track': {
+        opacity: 0.5,
+      },
+    },
+    '&.Mui-focusVisible .MuiSwitch-thumb': {
+      color: '#33cf4d',
+      border: '6px solid #fff',
+    },
+    '&.Mui-disabled .MuiSwitch-thumb': {
+      color:
+        theme.palette.mode === 'light'
+          ? theme.palette.grey[100]
+          : theme.palette.grey[600],
+    },
+    '&.Mui-disabled + .MuiSwitch-track': {
+      opacity: theme.palette.mode === 'light' ? 0.7 : 0.3,
+    },
+  },
+  '& .MuiSwitch-thumb': {
+    boxSizing: 'border-box',
+    width: 17,
+    height: 17,
+  },
+  '& .MuiSwitch-track': {
+    borderRadius: 26 / 2,
+    backgroundColor: theme.palette.mode === 'light' ? '#E0E5F2' : '#eaeef7',
+    opacity: 1,
+    transition: theme.transitions.create(['background-color'], {
+      duration: 500,
+    }),
+  },
+}));
+
 
 const Profile = () => {
   const [user, setUser] = useState([]);
@@ -156,9 +207,11 @@ const Profile = () => {
                     {
                       Object.keys(state).map((key, index) => {
                         return (
-                          < FormControlLabel
+                          < FormControlLabel sx={{color:'#2B3674'}}
                           control={
-                            <Switch checked={state[index]?.[key]} onChange={handleChange} name={key} />
+                            
+                              <IOSSwitch sx={{ m: 1 }} checked={state[index]?.[key]} onChange={handleChange} name={key}/>
+                            
                           }
                           label={key}
                           />
